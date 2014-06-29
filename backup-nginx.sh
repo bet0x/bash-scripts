aa91b2741c91d0c9f4ddf6bd2a3d76687da36995
Joshua Grancell <jgrancell@users.noreply.github.com>
2014-06-29 17:23:04 -0400

    Version 1.0 Complete
    Completed the version 1.0 of this nginx backup script.

--- backup-nginx.sh
+++ backup-nginx.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
+# Nginx backup Script v1.0
 # Author - Josh Grancell
-# Purpose - Creates a .tar.gz backup of your webroot directory
-# and uploads it to your Amazon S3 account.
+# License - GNU GPL v3
 
-#Defining our folders
+#Defining the folders
+webserver_folder="/web"
+root_backup_director="/backup"
+
 month_folder=$(date +%Y-%m)
 date_folder=$(date +%F)
-destination_folder=/backup/"$month_folder"/"$date_folder"
+destination_folder="$root_backup_directory"/"$month_folder"/"$date_folder"
 s3_container="grancell-vps-backup"
-webroot="/web"
 
-#Archiving our file
+#Archiving the files
 archive_file="nginx-backup-$(date +%F).tar.gz"
 mkdir -p "$destination_folder"
-/bin/tar -czvf "$destination_folder"/"$archive_file" "$webroot" > /dev/null
+/bin/tar -czvf "$destination_folder"/"$archive_file" "$webserver_folder" > /dev/null
 
-#Logging our archiving
+#Updating out SSH MOTD and sending to Amazon S3
 if [ -e "$destination_folder"/"$archive_file" ]; then
         echo "Backup of nginx files complete for $(date +%F)" >> /var/log/backup.log
-        
-        #Sending the archive to Amazon S3 for data redundancy
         /usr/local/bin/aws s3 cp "$destination_folder"/"$archive_file" s3://"$s3_container"/"$month_folder"/"$date_folder"/"$archive_file" >> /var/log/backup-aws.log
 else
-        #Our backup failed - logging accordingly
         echo "Backup FAILED for nginx files for $(date +%F)" >> /var/log/backup.log
         if grep "No backups have failed recently" /var/log/backup-failures.log; then
                 echo "Backup failed for Nginx files for $(date +%F)" > /var/log/backup-failures.log
